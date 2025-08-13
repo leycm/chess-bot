b257bedaa442ca5719767c9e6196a4a596f3d12c
𝙂𝙪𝙣𝙎𝙬𝙚𝙖𝙩 <matthis.schock@web.de>
2025-08-13 22:58:12 +0200

    Data Shifting [Ley]

--- src/main/java/org/leycm/chessbot/gui/ChessJFrameGame.java
+++ src/main/java/org/leycm/chessbot/gui/ChessJFrameGame.java
@@ -3,12 +3,15 @@ package org.leycm.chessbot.gui;
 import org.leycm.chessbot.chess.ChessBoard;
 import org.leycm.chessbot.chess.Piece;
 import org.leycm.chessbot.chess.pieces.*;
+import org.leycm.chessbot.model.ChessModel;
+import org.leycm.chessbot.model.ModelLoader;
+import org.leycm.chessbot.model.MoveConverter;
 
 import javax.swing.*;
 import javax.swing.Timer;
 import java.awt.*;
+import java.io.IOException;
 import java.util.*;
-import java.util.List;
 
 public class ChessJFrameGame {
 
@@ -17,10 +20,11 @@ public class ChessJFrameGame {
     private static JPanel jSidePanel = new JPanel(new BorderLayout());
     private static ChessBoard currentBoard = new ChessBoard();
     private static HashMap<String, Color> theme = new HashMap<>();
+    private static ChessModel model = new ChessModel();
 
     private static JPanel boardPanel;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         currentBoard = new ChessBoard();
 
@@ -33,24 +37,43 @@ public class ChessJFrameGame {
             currentBoard.placePiece(piece, point.x, point.y);
         });
 
-        currentBoard.movePiece(2, 1, 2, 2);
-        if (currentBoard.getPiece(2, 2) != null) {
-            System.out.println("Has moved");
-        }
+        currentBoard.movePiece(6, 1, 6, 2);
+
+        model = ModelLoader.loadModel("model/trained/chess_model-1.1.2-R0-SNAPSHOT.model");
+
+        currentBoard.setWhiteTurn(true);
 
         createChessBoardUI();
-
         new Timer(250, e -> {
 
             jFrame.setVisible(true);
+            boolean team = true;
 
-            /*Piece piece = currentBoard.getPieces(true).get(new Random().nextInt(0, currentBoard.getPieces(true).size()));
-            int[] cord = Arrays.stream(piece.getValidFields()).findAny().orElse(null);
-            if (cord != null) {
-                System.out.println(piece.getName() + ": " + piece.getX() + " " + piece.getY() + " | " + cord[0] + " " + cord[1]);
-                currentBoard.movePiece(piece.getX(), piece.getY(), cord[0], cord[1]);
-            } else {
-                System.out.println("can't Load Piece: " + piece.getName());
+            int[] gameState = new int[65];
+            int[] levelArray = currentBoard.getLevelArray();
+
+            for (int i = 0; i < levelArray.length; i++) {
+                gameState[i] = levelArray[i];
+            }
+
+            gameState[64] = currentBoard.isWhiteTurn() ? 1 : 0;
+
+            int[] bestMove = MoveConverter.findBestMove(model, gameState);
+
+            currentBoard.movePiece(bestMove[0], bestMove[1], bestMove[2], bestMove[3]);
+            System.out.println(Arrays.toString(bestMove));
+
+            /*or (int i = 0; i < 2; i++) {
+
+                Piece piece = currentBoard.getPieces(team).get(new Random().nextInt(0, currentBoard.getPieces(team).size()));
+                int[] cord = Arrays.stream(piece.getValidFields()).findAny().orElse(null);
+                if (cord != null) {
+                    System.out.println(piece.getName() + ": " + piece.getX() + " " + piece.getY() + " | " + cord[0] + " " + cord[1]);
+                    currentBoard.movePiece(piece.getX(), piece.getY(), cord[0], cord[1]);
+                } else {
+                    System.out.println("can't Load Piece: " + piece.getName());
+                }
+                team = !team;
             }*/
 
             if (jFrame.getWidth() < 400 || jFrame.getHeight() < 250) {
@@ -70,12 +93,18 @@ public class ChessJFrameGame {
             currentBoard.getMoveHistory().forEach(move -> {
 
                 String movedColor = move.movedPiece().isWhite() ? "White" : "Black";
-                String capturedColor = move.capturedPiece().isWhite() ? "White" : "Black";
+
+                String capturedStringEnd = "";
+                if (move.capturedPiece() != null) {
+
+                    String capturedColor = move.capturedPiece().isWhite() ? "White" : "Black";
+                    capturedStringEnd = " " + move.capturedPiece().getName() + " [" + capturedColor + "] ";
+                }
 
                 String moveS = move.movedPiece().getName() + " [" + movedColor + "] " +
                         move.fromX() + ", " + move.fromY() + " ---> " +
                         move.toX() + ", " + move.toY() +
-                        move.capturedPiece().getName() + " [" + capturedColor + "] ";
+                        capturedStringEnd;
 
                 addStringToHistory(moveS);
             });
@@ -87,30 +116,32 @@ public class ChessJFrameGame {
 
         HashMap<Point, Piece> pieces = new HashMap<>();
 
-        pieces.put(new Point(0, 0), new RookChessPiece(true, board));
-        pieces.put(new Point(0, 1), new KnightChessPiece(true, board));
-        pieces.put(new Point(0, 2), new BishopChessPiece(true, board));
-        pieces.put(new Point(0, 3), new QueenChessPiece(true, board));
-        pieces.put(new Point(0, 4), new KingChessPiece(true, board));
-        pieces.put(new Point(0, 5), new BishopChessPiece(true, board));
-        pieces.put(new Point(0, 6), new KnightChessPiece(true, board));
+        // White
         pieces.put(new Point(0, 7), new RookChessPiece(true, board));
+        pieces.put(new Point(1, 7), new KnightChessPiece(true, board));
+        pieces.put(new Point(2, 7), new BishopChessPiece(true, board));
+        pieces.put(new Point(3, 7), new KingChessPiece(true, board));
+        pieces.put(new Point(4, 7), new QueenChessPiece(true, board));
+        pieces.put(new Point(5, 7), new BishopChessPiece(true, board));
+        pieces.put(new Point(6, 7), new KnightChessPiece(true, board));
+        pieces.put(new Point(7, 7), new RookChessPiece(true, board));
 
-        for (int x = 0; x < 8; x++) {
-            pieces.put(new Point(1, x), new PawnChessPiece(true, board));
+        for (int y = 0; y < 8; y++) {
+            pieces.put(new Point(y, 6), new PawnChessPiece(true, board));
         }
 
+        // Black
+        pieces.put(new Point(0, 0), new RookChessPiece(false, board));
+        pieces.put(new Point(1, 0), new KnightChessPiece(false, board));
+        pieces.put(new Point(2, 0), new BishopChessPiece(false, board));
+        pieces.put(new Point(3, 0), new KingChessPiece(false, board));
+        pieces.put(new Point(4, 0), new QueenChessPiece(false, board));
+        pieces.put(new Point(5, 0), new BishopChessPiece(false, board));
+        pieces.put(new Point(6, 0), new KnightChessPiece(false, board));
         pieces.put(new Point(7, 0), new RookChessPiece(false, board));
-        pieces.put(new Point(7, 1), new KnightChessPiece(false, board));
-        pieces.put(new Point(7, 2), new BishopChessPiece(false, board));
-        pieces.put(new Point(7, 3), new QueenChessPiece(false, board));
-        pieces.put(new Point(7, 4), new KingChessPiece(false, board));
-        pieces.put(new Point(7, 5), new BishopChessPiece(false, board));
-        pieces.put(new Point(7, 6), new KnightChessPiece(false, board));
-        pieces.put(new Point(7, 7), new RookChessPiece(false, board));
 
-        for (int x = 0; x < 8; x++) {
-            pieces.put(new Point(6, x), new PawnChessPiece(false, board));
+        for (int y = 0; y < 8; y++) {
+            pieces.put(new Point(y, 1), new PawnChessPiece(false, board));
         }
 
         return pieces;
@@ -152,7 +183,7 @@ public class ChessJFrameGame {
 
         boardPanel.removeAll();
 
-        for (int row = 7; row >= 0; row--) {
+        for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 JPanel square = new JPanel(new BorderLayout());
                 boolean isBlack = (row + col) % 2 != 0;
@@ -170,7 +201,7 @@ public class ChessJFrameGame {
                 String text = points.get(new Point(col, row));
                 if (text != null) {
                     JLabel label = new JLabel(text, SwingConstants.CENTER);
-                    Piece piece = currentBoard.getPiece(row, col);
+                    Piece piece = currentBoard.getPiece(col, row);
                     if (piece != null) {
                         label.setForeground(piece.isWhite() ? Color.WHITE : Color.BLACK);
                     } else {
